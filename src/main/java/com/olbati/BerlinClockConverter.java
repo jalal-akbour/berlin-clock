1e5a3a488e36076ab4a8325b7df6ac4053921098
Jalal AKBOUR <jalal.akbour@gmail.com>
2019-11-07 18:23:52 +0100

    Five Minutes Row Tested and No regression found

--- src/main/java/com/olbati/BerlinClockConverter.java
+++ src/main/java/com/olbati/BerlinClockConverter.java
@@ -5,6 +5,10 @@ import java.time.format.DateTimeFormatter;
 
 public class BerlinClockConverter {
 
+    final static private String yellowLamp = "Y";
+    final static private String redLamp = "R";
+    final static private String offLamp = "O";
+
     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
 
     public BerlinClock digital_to_berlin_convertor (String digitalTimeValue) {
@@ -14,6 +18,9 @@ public class BerlinClockConverter {
 
         String singleMinutesRow = calculate_single_minutes_row(time.getMinute());
         berlinClockValue.setSingleMinutesRow(singleMinutesRow);
+
+        String fiveMinutesRow = calculate_five_minutes_row(time.getMinute());
+        berlinClockValue.setFiveMinutesRow(fiveMinutesRow);
 
         return berlinClockValue;
     }
@@ -29,6 +36,25 @@ public class BerlinClockConverter {
 
         while (singleMinutesRow.length() < 4) {
             singleMinutesRow.append("O");
+        }
+        return singleMinutesRow.toString();
+    }
+
+    public String calculate_five_minutes_row (int digitalTimeMinutes) {
+
+        int singleMinutesValue = digitalTimeMinutes / 5;
+        StringBuilder singleMinutesRow = new StringBuilder();
+
+        for (int i = 1; i <= singleMinutesValue; i++) {
+            if (i % 3 == 0) {
+                singleMinutesRow.append(redLamp);
+            } else {
+                singleMinutesRow.append(yellowLamp);
+            }
+        }
+
+        while (singleMinutesRow.length() < 11) {
+            singleMinutesRow.append(offLamp);
         }
         return singleMinutesRow.toString();
     }
--- src/test/java/com/olbati/BerlinClockFiveMinutesRowTest.java
+++ src/test/java/com/olbati/BerlinClockFiveMinutesRowTest.java
@@ -1,4 +1,53 @@
 package com.olbati;
 
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class BerlinClockFiveMinutesRowTest {
+
+    // Arrange
+    BerlinClockConverter berlinClockConverter = new BerlinClockConverter();
+    BerlinClock berlinClockTime;
+
+    @Test
+    public void when_digital_time_equal_to_00_00_00_then_berlin_time_five_minutes_row_equal_OOOOOOOOOOO () {
+        // Act
+        berlinClockTime = berlinClockConverter.digital_to_berlin_convertor("00:00:00");
+        // Assert
+        assertThat(berlinClockTime.getFiveMinutesRow()).isEqualTo("OOOOOOOOOOO");
+    }
+
+    @Test
+    public void when_digital_time_equal_to_23_59_59_then_berlin_time_five_minutes_row_equal_YYRYYRYYRYY () {
+        // Act
+        berlinClockTime = berlinClockConverter.digital_to_berlin_convertor("23:59:59");
+        // Assert
+        assertThat(berlinClockTime.getFiveMinutesRow()).isEqualTo("YYRYYRYYRYY");
+    }
+
+    @Test
+    public void when_digital_time_equal_to_12_04_00_then_berlin_time_five_minutes_row_equal_OOOOOOOOOOO () {
+        // Act
+        berlinClockTime = berlinClockConverter.digital_to_berlin_convertor("12:04:00");
+        // Assert
+        assertThat(berlinClockTime.getFiveMinutesRow()).isEqualTo("OOOOOOOOOOO");
+    }
+
+    @Test
+    public void when_digital_time_equal_to_12_23_00_then_berlin_time_five_minutes_row_equal_YYRYOOOOOOO () {
+        // Act
+        berlinClockTime = berlinClockConverter.digital_to_berlin_convertor("12:23:00");
+        // Assert
+        assertThat(berlinClockTime.getFiveMinutesRow()).isEqualTo("YYRYOOOOOOO");
+    }
+
+    @Test
+    public void when_digital_time_equal_to_12_35_00_then_berlin_time_five_minutes_row_equal_YYRYYRYOOOO () {
+        // Act
+        berlinClockTime = berlinClockConverter.digital_to_berlin_convertor("12:35:00");
+        // Assert
+        assertThat(berlinClockTime.getFiveMinutesRow()).isEqualTo("YYRYYRYOOOO");
+    }
+
 }
